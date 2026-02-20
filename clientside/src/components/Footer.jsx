05570f135a0c70c82208d9d405cdedc1df2cc301
MD ZAFAR <mz632359@gmail.com>
2026-02-20 07:34:37 +0530

    Add files via upload

---
+++ clientside/src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        {/* ------------ Left Section ------------ */}
+        <div>
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            Our vision at MediLab+ is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.
+          </p>
+        </div>
+
+        {/* ------------ Center Section ------------ */}
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        {/* ------------ Right Section ------------ */}
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>+91-9876543210</li>
+            <li>MediLabPlus@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* ------------ Copyright Text ------------ */}
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright © 2026 MediLab+ All Right Reserved
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
